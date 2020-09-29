119ea06bb8a40f35b8681a13efc836315e4416a9
ErikPereira <erik_bezerra_silva@hotmail.com>
2020-09-29 10:52:35 -0300

    29/09: pasta e arquivo Sintatico criados

---
+++ src/main/java/Sintatico/Sintatico.java
@@ -0,0 +1,111 @@
+
+package Sintatico;
+import Lexico.Lexico;
+
+public class Sintatico {
+    private Lexico lexico;
+    
+    void Sintatico(String programa){
+        this.lexico = new Lexico(programa);
+    }
+    
+    public void analisadorSintatico(){
+        
+    }
+    
+    private void lexico(){
+    
+    }
+    
+    private void analisaBloco(){
+    
+    }
+    
+    private void analisaEtVariaveis(){
+    
+    }
+    
+    private void analisaVariaveis(){
+    
+    }
+    
+    private void analisaTipo(){
+    
+    }
+    
+    private void analisaComandos(){
+    
+    }
+    
+    private void analisaComandoSimples(){
+    
+    }
+    
+    private void analisaAtribChprocedimento(){
+    
+    }
+    
+    private void analisaLeia(){
+    
+    }
+    
+    private void analisaEscreva(){
+    
+    }
+    
+    private void analisaEnquanto(){
+    
+    }
+    
+    private void analisaSe(){
+    
+    }
+    
+    private void analisaSubrotinas(){
+    
+    }
+    
+    private void analisaDeclaraçãoProcedimento(){
+    
+    }
+    
+    private void analisaDeclaraçãoFunção(){
+    
+    }
+    
+    private void analisaExpressao(){
+    
+    }
+    
+    private void analisaExpressãoSimples(){
+    
+    }
+    
+    private void analisaTermo(){
+    
+    }
+    
+    private void analisaFator(){
+    
+    }
+    
+    private void analisaChamadaDeProcedimento(){
+    
+    }
+    
+    private void analisaChamadaDeFuncao(){
+    
+    }
+    
+    private void analisaAtribuicao(){
+    
+    }
+    
+    private void erro(){
+    
+    }
+    
+    private void sucesso(){
+    
+    }
+}
